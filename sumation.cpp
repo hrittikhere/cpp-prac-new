e722af4bdd823da7b16d56911ce4c172f8c3f926
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-01-12 02:05:38 +0530

    Create sumation.cpp

---
+++ sumation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+long double PI = acos(-1.0);
+
+void solve()
+{
+	int final;
+	cin >> final;
+	cout << (final * (final + 1)) / 2 << endl;
+
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	// int _t; cin >> _t; while (_t--)
+	solve();
+
+}
