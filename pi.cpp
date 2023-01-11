d57c9e8385a4ed59f50d348f8b1bf9e908e4734d
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-01-12 02:03:40 +0530

    Create pi.cpp

---
+++ pi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+long double PI = acos(-1.0);
+
+void solve()
+{
+	long double radius;
+	cin >> radius;
+	cout << radius*radius*PI;
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	// int _t; cin >> _t; while (_t--)
+	solve();
+
+}
